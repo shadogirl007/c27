08403a8e29d6518f079d980786d43e8d9aa0d379
Dhriti Agarwal <dhriti@Dhritis-MacBook-Air.local>
2020-10-07 19:54:16 +0530

    constraint

---
+++ chain.js
@@ -0,0 +1,18 @@
+class Chain{
+    constructor(body1, body2){
+        var options={
+            bodyA: body1,
+            bodyB: body2,
+            stiffness: 0.1,
+            length: 30
+        }
+        this.chain= Constraint.create(options)
+        World.add(world, this.chain);
+    }
+    display(){
+        strokeWeight(4);
+        line(this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y);
+
+    }
+  
+}
